9a3bedf006149790a5aebc3b7c189c01895330a0
ivica-kolenkas-devtech <ivica.kolenkas@gmail.com>
2017-02-25 22:59:44 +0100

    added checking for multiple domains, better response

--- domain/service.py
+++ domain/service.py
@@ -3,28 +3,48 @@ from ._apiwrapper import DomainAPIWrapper
 
 class DomainService:
 	def __init__(self):
-		self.__base_url = "http://sandbox.namesilo.com/api/"
-
-		self.api = DomainAPIWrapper()
+		self._api = DomainAPIWrapper()
 
 	@staticmethod
 	def _parse_response(api_response_content):
 		payload = api_response_content["namesilo"]["reply"]
 
-		available_domains = payload.get("available").get("domain", [])
-		unavailable_domains = payload.get("unavailable").get("domain", [])
-		invalid_domains = payload.get("invalid").get("domain", [])
+		available_domains = payload.get("available", {}).get("domain", None)
+		unavailable_domains = payload.get("unavailable", {}).get("domain", None)
+		invalid_domains = payload.get("invalid", {}).get("domain", None)
 
 		return available_domains, unavailable_domains, invalid_domains
 
-	def check_availability(self, *args):
-		domains = ",".join(args)
 
-		api_response = self.api.check_availability(domains)
+	def check_availability(self, domain):
+		domain_available = False
+
+		api_response = self._api.check_availability(domain)
 
 		if api_response.success:
 			available, unavailable, invalid = self._parse_response(api_response.content)
 
-			return available, unavailable, invalid
+			if available == domain:
+				domain_available = True
+
+			return domain_available
+		else:
+			raise Exception(api_response.error)
+
+	def bulk_check_availability(self, *args):
+		domains = ",".join(args)
+		response = {}
+		api_response = self._api.check_availability(domains)
+
+		if api_response.success:
+			available, unavailable, invalid = self._parse_response(api_response.content)
+
+			for domain in args:
+				if domain in available:
+					response[domain] = True
+				else:
+					response[domain] = False
+
+			return response
 		else:
 			raise Exception(api_response.error)
